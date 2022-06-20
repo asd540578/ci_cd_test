c6861362503739c700590660904742e6566deda2
丹丹 <asd5405789789@gmail.com>
2022-06-20 18:07:29 +0800

    第一個測試

---
+++ src/test/java/com/dan/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.dan;
+
+import com.App;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    public void canRunApplication() {
+        App.main(new String[]{});
+    }
+}
